ee7d0754924569a0cda6da178f27cb318e940f32
Hampus Isebring <hampus.isebring@gmail.com>
2024-11-16 04:51:28 +0100

    update

--- src/components/Parallax.tsx
+++ src/components/Parallax.tsx
@@ -3,6 +3,7 @@ import SimpleParallax from "simple-parallax-js";
 import parallaxImg1 from "../assets/imgs/history-pattern.jpg";
 import parallaxImg from "../assets/imgs/history-pattern1.jfif";
 import parallaxImg2 from "../assets/imgs/history-pattern2.jfif";
+import parallaxImg3 from "../assets/imgs/history-pattern3.jfif";
 import parallaxImg4 from "../assets/imgs/history-pattern4.jfif";
 import "../css/Parallax.css";
 
@@ -13,19 +14,30 @@ const contentSections = [
     image: parallaxImg1,
   },
   {
-    title: "Evidence from Assigned Reading",
-    text: "Explain how specific reading evidence supports my thesis.",
+    title: "Evidence from Assigned Reading #1",
+    quote:
+      "“On the part of the slave, this attachment is manifested by an affection and faithfulness which only cease with life.” - Joseph Holt Ingraham ",
+    text: "Ingraham’s claim that enslaved individuals displayed “affection and faithfulness which only cease with life” reflects a romanticized narrative designed to justify slavery. By portraying enslaved people as content and loyal, this narrative obscures the brutality of their oppression and invalidates their resistance, erasing their humanity. Such distortions supported the glorified view of southern prosperity while marginalizing African Americans' experiences. This reinforces my understanding of how historical injustices, like slavery, were justified and how these narratives continue to shape modern debates on systemic racism and inequality, perpetuating myths that minimize the lasting impact of these injustices.",
     image: parallaxImg4,
   },
   {
-    title: "Reflection Evidence - Self",
-    text: "Summary from your own reflections and its connection to my thesis.",
+    title: "Evidence from Assigned Reading #2",
+    quote:
+      "“Families at dinner were startled by the sudden gleam of bayonets in the doorway and rose up to be driven with blows and oaths along the weary miles of trail that led to the stockade. Men were seized in their fields or going along the road, women were taken from their wheels and children from their play. In many cases, on turning for one last look as they crossed the ridge, they saw their homes in flames.” - James Mooney",
+    text: "Mooney’s account of Cherokee families being forcibly removed from their homes, with some witnessing their houses burned as they were driven to stockades, reveals the brutal realities of westward expansion. This violent displacement undermines the glorified narratives of expansion as a noble endeavor, highlighting instead how progress for settlers came at the direct expense of Native Americans’ dignity and survival. The suffering described in this passage exemplifies the overlooked injustices against marginalized groups like the Cherokee, whose forced removal has a lasting legacy in contemporary struggles over Indigenous rights and recognition.",
     image: parallaxImg2,
   },
   {
-    title: "Reflection Evidence - Classmates",
-    text: " Specific references from Reflection assignments of my classmates",
+    title: "Evidence from Assigned Reading #3",
+    quote: "",
+    text: " Specific references from the assigned reading.",
     image: parallaxImg,
+  },
+  {
+    title: "Evidence from Assigned Reading #4",
+    quote: "",
+    text: "Specific references from the assigned reading.",
+    image: parallaxImg3,
   },
 ];
 
@@ -66,14 +78,14 @@ function Parallax() {
           >
             <Image
               style={{
-                backgroundImage: `linear-gradient(to bottom, rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0)), url(${section.image})`,
+                backgroundImage: `url(${section.image})`,
                 backgroundRepeat: "no-repeat",
                 backgroundSize: "cover",
                 backgroundAttachment: "fixed",
                 backgroundPosition: "center center",
                 width: "100%",
                 height: "100vh",
-                objectFit: "cover",
+                // objectFit: "",
               }}
             />
           </SimpleParallax>
@@ -101,6 +113,16 @@ function Parallax() {
             >
               {section.title}
             </Title>
+            <Text
+              mb="md"
+              style={{
+                fontFamily: "Cinzel, serif",
+                fontSize: "clamp(1rem, 1.5vw, 1.25rem)",
+                fontStyle: "italic",
+              }}
+            >
+              {section.quote}
+            </Text>
             <Text
               style={{
                 fontFamily: "Cinzel, serif",
